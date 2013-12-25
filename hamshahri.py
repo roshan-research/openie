d06997e54080103fd0ec0a2fde7a1f9e3e81ad54
Alireza Nourian <alireza.nournia@gmail.com>
2013-12-25 09:01:15 +0330

    accumulate texts for parsing

--- hamshahri.py
+++ hamshahri.py
@@ -1,6 +1,7 @@
 
 from hazm import sent_tokenize, word_tokenize, Normalizer, HamshahriReader, POSTagger, DependencyParser
 from InformationExtractor import InformationExtractor
+from progress.bar import Bar
 
 
 hamshahri = HamshahriReader()
@@ -8,18 +9,20 @@ normalizer = Normalizer()
 tagger = POSTagger()
 parser = DependencyParser(tagger=tagger)
 extractor = InformationExtractor()
+texts = []
 
 output = open('informations.txt', 'w')
-for text in hamshahri.texts():
-	text = normalizer.normalize(text)
-	sentences = [word_tokenize(sentence) for sentence in sent_tokenize(text)]
+for text in Bar(max=310000).iter(hamshahri.texts()):
+	texts.append(normalizer.normalize(text))
+	if len(texts) <= 1000: continue
+
+	sentences = sum([[word_tokenize(sentence) for sentence in sent_tokenize(text)] for text in texts], [])
 	tagged = tagger.batch_tag(sentences)
 	parsed = parser.tagged_batch_parse(tagged)
+	texts = []
 
 	for sentence in parsed:
-		print('\n', '*', *[node['word'] for node in sentence.nodelist if node['word']], file=output)
+		# print('*', *[node['word'] for node in sentence.nodelist if node['word']], file=output)
 		for information in extractor.extract(sentence):
 			print(*information, sep=' - ', file=output)
-
-	break
-
+		print(file=output)

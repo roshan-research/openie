44cf40f60b1d295a5e596fa7421ae759e5a240c9
imani <imany.mohsen@gmail.com>
2015-01-20 16:13:59 +0330

    learn dadegan dadegan extraction with accuracy of: 0.7575

--- baaz/LearningBasedInformationExtractor.py
+++ baaz/LearningBasedInformationExtractor.py
@@ -3,12 +3,43 @@ import codecs
 
 
 tagger = IOBTagger(patterns=[
-		'u:t=%X[0,1]',
-		'u:c=%X[0,2]',
-		'u:a=%X[0,3]'
+		'*',
+
+		'u:w=%X[0,0]',
+		'u:wl1=%X[-1,0]',
+		'u:wr1=%X[1,0]',
+		#'u:wl2=%X[-2,0]',
+		#'u:wr2=%X[2,0]',
+
+		'*:tl1=%X[-1,1]',
+		#'*:tl2=%X[-2,1]',
+		#'*:tl3=%X[-3,1]',
+		'*:t=%X[0,1]',
+		'*:tr1=%X[1,1]',
+		#'*:tr2=%X[2,1]',
+		#'*:tr3=%X[3,1]',
+
+		'*:cp=%m[0,2,"..$"]',
+		'*:c=%X[0,2]',
+
+		'*:c0l1=%X[-1,2]/%X[0,2]',
+		'*:c0r1=%X[0,2]/%X[1,2]',
+
+		'*:cl1=%X[-1,2]',
+		'*:cl2=%X[-2,2]',
+		#'*:cl3=%X[-3,2]',
+		#'*:cl4=%X[-4,2]',
+		#'*:cl5=%X[-5,2]',
+		#'*:cl6=%X[-6,2]',
+		'*:cr1=%X[1,2]',
+		'*:cr2=%X[2,2]',
+		#'*:cr3=%X[3,2]',
+		#'*:cr4=%X[4,2]',
+		#'*:cr5=%X[5,2]',
+		#'*:cr6=%X[6,2]',
 	])
 
-training = codecs.open('trainingSet.txt', 'r', encoding='utf8').read()
+training = codecs.open('trainingSet_dadegan.txt', 'r', encoding='utf8').read()
 
 sentences = []
 
@@ -23,31 +54,29 @@ for i, raw_sentence in enumerate(training.split('\n\n')):
 
 
 
-#THRESHOLD = int(len(sentences) * 0.9)
-tagger.train(sentences)
+THRESHOLD = int(len(sentences) * 0.9)
+tagger.train(sentences[:THRESHOLD])
 tagger.save_model('informations.model')
+"""
 test = []
-for sentence in sentences:
+for sentence in sentences[THRESHOLD:]:
 	test_sent = []
 	for token in sentence:
-		if len(token) < 5:
-			print(token)
 		test_sent.append(token[:-1])
 	test.append(test_sent)
 
 tagged_sents = tagger.tag_sents(test)
 corrects = 0
 total = 0
-for tagged, gold in zip(tagged_sents, sentences[:1]):
+for tagged, gold in zip(tagged_sents, sentences[THRESHOLD:]):
 	for tagged_token, gold_token in zip(tagged, gold):
-		if tagged_token[-1] != 'O':
-			print(tagged)
 		if tagged_token[-1] == gold_token[-1]:
 			corrects += 1
 		total += 1
 
 print(corrects / total)
-
-print(sentences[1])
+"""
+print(tagger.evaluate(sentences[THRESHOLD:]))
+print(sentences[0])
 print()
-print(tagger.tag(test[1]))
+#print(tagger.tag(test[0]))

a570224d47d49c09d2234d83be3c8dd2c8a2e042
Alireza Nourian <az.nourian@gmail.com>
2015-09-19 14:42:43 +0430

    don't remove punctuations from arguments

--- baaz/DependencyTreeInformationExtractor.py
+++ baaz/DependencyTreeInformationExtractor.py
@@ -1,15 +1,24 @@
 
 from itertools import product
+from hazm import *
 
 
 class DependencyTreeInformationExtractor():
 	""" extracts information from dependency tree """
 
+	def __init__(self):
+		self.normalizer = Normalizer(punctuation_spacing=True, character_refinement=False, affix_spacing=False)
+
+	def phrase(self, nodes):
+		words = self.normalizer.normalize(' '.join([node['word'] for node in sorted(nodes, key=lambda n: n['address'])]))
+		if words[-1] in {'.', ':', '?', '!', '،', '؟'}:
+			words = words[:-1].strip()
+		return words
+
 	def extract(self, tree):
 		nodes = list(tree.nodes.values())
 		childs = lambda parent, rels=None: [nodes[n] for n in sum([parent['deps'][rel] for rel in parent['deps'] if not rels or rel == rels or rel in rels], [])]
 		subtree = lambda parent: sum([subtree(child) for child in childs(parent)], [parent])
-		words = lambda nodes: ' '.join([node['word'] for node in sorted(nodes, key=lambda n: n['address']) if node['rel'] != 'PUNC'])
 
 		verbs = [node for node in nodes if node['ctag'] == 'V']
 		for verb in verbs:
@@ -38,4 +47,4 @@ class DependencyTreeInformationExtractor():
 
 			# yield results
 			for arg1, arg2 in product(arg1s, arg2s):
-				yield list(map(words, (subtree(arg1), subtree(arg2), relation)))
+				yield list(map(self.phrase, (subtree(arg1), subtree(arg2), relation)))
